19e529babea0a7d897f87f9e88ab33fc2e5b024b
dahamkakooza <k.mahad@alustudent.com>
2024-10-25 19:55:52 +0000

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""DOCS"""
+"""Reddit API Query"""
 import requests
 
-
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
+    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    
+    # Sending GET request without following redirects
+    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
 
+    # Check if the response status is 200 (OK)
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        try:
+            data = response.json().get('data', {})
+            children = data.get('children', [])
+            if not children:
+                print(None)
+                return
+            # Print the titles of the first 10 hot posts
+            for post in children[:10]:
+                print(post['data']['title'])
+        except (KeyError, ValueError):
+            print(None)
     else:
-        print(OK)
+        print(None)
