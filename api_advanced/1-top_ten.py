af1c8019e679f94418d76cb208d4a3fa256799f7
dahamkakooza <k.mahad@alustudent.com>
2024-10-25 20:01:33 +0000

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,26 +2,17 @@
 """DOCS"""
 import requests
 
+
 def top_ten(subreddit):
     """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-agent': 'Mozilla/5.0'}
-    
-    # Sending GET request without following redirects
-    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
 
-    # Check if the response status is 200 (OK)
     if response.status_code == 200:
-        try:
-            data = response.json().get('data', {})
-            children = data.get('children', [])
-            if not children:
-                print(None)
-                return
-            # Print the titles of the first 10 hot posts
-            for post in children[:10]:
-                print(post['data']['title'])
-        except (KeyError, ValueError):
-            print(None)
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
     else:
         print(None)
