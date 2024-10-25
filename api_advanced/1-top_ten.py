8b7ff469aa4c33d5c624702262edb84ec4fec9c2
dahamkakooza <k.mahad@alustudent.com>
2024-10-25 19:51:56 +0000

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,21 +2,17 @@
 """DOCS"""
 import requests
 
+
 def top_ten(subreddit):
-    """Print the top ten hot posts of a given subreddit."""
-    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json().get('data', {})
-        if 'children' in data:
-            for post in data['children'][:10]:
-                print(post['data']['title'])
-            return "OK"  # Indicating successful completion
-        else:
-            print(None)
-            return "None"  # In case data is empty or not formatted as expected
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
     else:
-        print(None)
-        return "None"  # Indicating failure due to a non-200 status code
+        print(OK)
